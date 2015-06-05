ec1685678b877e82ac838645b9e300da027f7d9f
phauer <info@philipphauer.de>
2015-06-05 16:05:03 +0200

    added project scaffold for trying the swagger generated client.

---
+++ prozu-simpleclient/src/test/java/de/philipphauer/prozu/simpleclient/SwaggerClientTest.java
@@ -0,0 +1,12 @@
+package de.philipphauer.prozu.simpleclient;
+
+import org.junit.Test;
+
+public class SwaggerClientTest {
+
+	@Test
+	public void test() {
+		//TODO test swagger-generated client here...
+		System.out.println("test!");
+	}
+}
