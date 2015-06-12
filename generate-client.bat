a583b5708de0f81d51cbc01f14ab0787afc106e9
phauer <info@philipphauer.de>
2015-06-12 13:37:28 +0200

    deleted simpleclient project, instead we generate the whole swagger client as a standalone project

---
+++ generate-client.bat
@@ -0,0 +1,15 @@
+SET "FILENAME=%~dp0\swagger-codegen-cli.jar"
+SET "PROJECTNAME=prozu-swagger-client"
+
+if not exist "%FILENAME%" (
+	bitsadmin /transfer "Downloading swagger-codegen jar" http://search.maven.org/remotecontent?filepath=io/swagger/swagger-codegen-cli/2.1.2/swagger-codegen-cli-2.1.2.jar "%FILENAME%"
+) 
+
+echo "you have to start the service first for accessing the swagger.json"
+java -jar "%FILENAME%" generate -i http://localhost:8080/swagger.json -l java -c ./generate-client-config.json -o ./%PROJECTNAME%
+
+pause
+
+cd ./%PROJECTNAME%
+mvn install
+
