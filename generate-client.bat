0c41e6fd4570c9d0ba763f6a9f450327ae79586c
phauer <info@philipphauer.de>
2015-07-17 11:29:45 +0200

    generating swagger.json and swagger documentation in build. using this swagger.json for client lib generation.

--- generate-client.bat
+++ generate-client.bat
@@ -1,12 +1,18 @@
+rem how to get swagger.json?
+rem a) start service and use http://localhost:8080/swagger.json
+rem b) build service project and use .\prozu-service\target\swagger\swagger-ui\swagger.json
+
 SET "FILENAME=%~dp0\swagger-codegen-cli.jar"
-SET "PROJECTNAME=prozu-swagger-client"
+SET "PROJECTNAME=prozu-service-client"
+
+rd /s /q "%PROJECTNAME%"	
 
 if not exist "%FILENAME%" (
 	bitsadmin /transfer "Downloading swagger-codegen jar" http://search.maven.org/remotecontent?filepath=io/swagger/swagger-codegen-cli/2.1.2/swagger-codegen-cli-2.1.2.jar "%FILENAME%"
 ) 
 
 echo "you have to start the service first for accessing the swagger.json"
-java -jar "%FILENAME%" generate -i http://localhost:8080/swagger.json -l java -c ./generate-client-config.json -o ./%PROJECTNAME%
+java -jar "%FILENAME%" generate -i ./prozu-service/target/swagger/swagger-ui/swagger.json -l java -c ./generate-client-config.json -o ./%PROJECTNAME%
 
 cd ./%PROJECTNAME%
 mvn install
