d9e159988c46f70ebcbf5fb98ef1448e9fe6fca6
phauer <info@philipphauer.de>
2015-12-05 20:50:06 +0100

    added  simple spring boot application

---
+++ musician-service/src/main/java/de/philipphauer/musicianservice/MusicianController.java
@@ -0,0 +1,22 @@
+package de.philipphauer.musicianservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class MusicianController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MusicianController.class, args);
+    }
+}
